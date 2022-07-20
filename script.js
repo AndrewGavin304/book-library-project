0d4b61013473b701863dab66d8ea39b8fb4f4099
AndrewGavin304 <AndrewGavin304@gmail.com>
2022-07-20 16:19:10 -0400

    Complete library layout; adding books to library

--- script.js
+++ script.js
@@ -10,12 +10,14 @@ class Book {
 }
 
 addBookToLibrary = () => {
-  title = prompt('What is the book\'s title?')
-  author =  prompt('What is the book\'s author\'s name?')
-  pages = prompt('How many pages is the book?')
-  beenRead = prompt('Have you read it?')
+  const title = document.querySelector('.title-input').value
+  const author =  document.querySelector('.author-input').value
+  const pages = document.querySelector('.pages-input').value
+  const beenRead = true;
   myLibrary.push(new Book(title, author, pages, beenRead));
   createCard(myLibrary[myLibrary.length - 1])
+  showAddBookForm()
+  clearAddBookForm()
 }
 
 showAddBookForm = () => {
@@ -26,10 +28,14 @@ showAddBookForm = () => {
   else {
     addBookForm.style.display = 'none';
   }
-
 }
 
-const addBookBtn = document.querySelector(".add-book-wrapper__btn");
+clearAddBookForm = () => {
+  document.querySelector('.title-input').value = ''
+  document.querySelector('.author-input').value = ''
+  document.querySelector('.pages-input').value = ''
+}
+const addBookBtn = document.querySelector(".header__new-book");
 addBookBtn.addEventListener('click', showAddBookForm);
 
 myLibrary.push(new Book('Infinite Jest', 'David Foster Wallace', '852', 'Yes'))
@@ -39,28 +45,38 @@ function initializeLibrary() {
   myLibrary.forEach(book => createCard(book));
 }
 
+initializeLibrary();
+
 function createCard(book) {
   let library = document.querySelector(".library");
-  const newDiv = document.createElement("div");
-  newDiv.classList.add('library__card');
-  library.appendChild(newDiv);
+
+  const outerDiv = document.createElement("div");
+  outerDiv.classList.add('library__card-outer')
+  library.appendChild(outerDiv);
+
+  const innerDiv = document.createElement("div");
+  innerDiv.classList.add('library__card-inner');
+  outerDiv.appendChild(innerDiv)
 
   const cardHeader = document.createElement("span");
   cardHeader.textContent = book.title;
   cardHeader.classList.add('library__title');
-  newDiv.appendChild(cardHeader);
+  innerDiv.appendChild(cardHeader);
 
   const cardSubHeader = document.createElement("span");
   cardSubHeader.textContent = book.author;
   cardSubHeader.classList.add('library__author');
-  newDiv.appendChild(cardSubHeader);
+  innerDiv.appendChild(cardSubHeader);
 
   const cardBookLength = document.createElement("span");
   cardBookLength.textContent = `${book.pages} pages`;
   cardBookLength.classList.add('library__pages');
-  newDiv.appendChild(cardBookLength);
+  // innerDiv.appendChild(cardBookLength);
 
-  // const cardReadNotifier = document.createElement("span")
-  // cardReadNotifier.textContent = book.beenRead;
-  // cardReadNotifier.classList.add('')
+  const read = document.querySelector('.read-input')
+  if (read.checked) {
+    const readIndicator = document.createElement("div");
+    readIndicator.classList.add('library__card-read');
+    outerDiv.appendChild(readIndicator);
+  }
 }
